9541c2903e8a14ee3a71da48e60bd8f1ad0a6cc4
Cedric Reber <cedric.reber@gmail.com>
2020-03-04 15:51:40 +0100

    S01_IR_record: removed last empty column in csv. Adapted plotter.py accordingly.

--- controllers/S01_IR_record/plot_calibration.py
+++ controllers/S01_IR_record/plot_calibration.py
@@ -1,36 +1,32 @@
-#********************************************************************************************
+# ********************************************************************************************
 # Calibration Plot Demo
-# 
+#
 # Description: Plot data from CSV file.
-#********************************************************************************************
+# ********************************************************************************************
 from pandas import *
 import pandas
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_sensors(csv,name) :
-    # drop last empty column
-    csv.drop(csv.columns[8], axis=1, inplace = True)
+
+def plot_sensors(csv, name):
     # plot data with subplots
-    csv.plot(subplots=True,sharey='col')
-    plt.savefig(name+'sub.png')
-    plt.savefig(name+'sub.pdf')
+    csv.plot(subplots=True, sharey='col')
+    plt.savefig(name + 'sub.png')
+    plt.savefig(name + 'sub.pdf')
     plt.show()
     # plot data on single plot
     csv.plot()
     # set the legend on right corner
     plt.legend(loc='upper right')
-    plt.savefig(name+'.png')
-    plt.savefig(name+'.pdf')
-    plt.show() 
+    plt.savefig(name + '.png')
+    plt.savefig(name + '.pdf')
+    plt.show()
+
 
 # get data from CSV file
 uncalib = read_csv('uncalib.csv', index_col=0)
-plot_sensors(uncalib,"uncalib")
+plot_sensors(uncalib, "uncalib")
 
 calib = read_csv('calib.csv', index_col=0)
-plot_sensors(calib,"calib")
-
-
-
-
+plot_sensors(calib, "calib")
