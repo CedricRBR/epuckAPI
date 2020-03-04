3c6e97045f23a7fd989251c0b543c0625810e76e
Cedric Reber <cedric.reber@gmail.com>
2020-03-04 15:49:46 +0100

    Added header files to all controllers. Slight cosmetic changes to API.

--- controllers/S01_basic_lover/S01_basic_lover.c
+++ controllers/S01_basic_lover/S01_basic_lover.c
@@ -1,47 +1,40 @@
+#include "S01_basic_lover.h"
 
-#define SIMULATION 0
-
-#if SIMULATION
-#include "../API/webots/webotsAPI.h"  
-#else
-#include "../API/epuck/epuckAPI.h"
-#endif
-
-void robot_setup() {
-    init_robot();
-    init_sensors();
-    calibrate_prox();
+void robot_setup()
+{
+  init_robot();
+  init_sensors();
+  calibrate_prox();
 }
 
-void robot_loop() {
-	short int prox_values[8];
+void robot_loop()
+{
+  short int prox_values[8];
 
-    while (robot_go_on()) {
+  while (robot_go_on())
+  {
 
-        // fetch proximity sensor values 
-        get_prox_calibrated(prox_values);
-        
-        // basic love behavior
-        double prox_front = (prox_values[PROX_RIGHT_FRONT] + prox_values[PROX_LEFT_FRONT]) / 2.;
-        double ds = - (NORM_SPEED * prox_front) / MAX_PROX;
-        double speed = NORM_SPEED + ds;
+    // fetch proximity sensor values
+    get_prox_calibrated(prox_values);
 
-        // make sure speed values are legal
-        set_speed(bounded_speed(speed),bounded_speed(speed));
-    }
-    cleanup_robot();
-}
+    // basic love behavior
+    double prox_front = (prox_values[PROX_RIGHT_FRONT] + prox_values[PROX_LEFT_FRONT]) / 2.;
+    double ds         = -(NORM_SPEED * prox_front) / MAX_PROX;
+    double speed      = NORM_SPEED + ds;
 
-int main (int argc, char **argv) {
+    // make sure speed values are legal
+    set_speed(bounded_speed(speed), bounded_speed(speed));
+  }
+  cleanup_robot();
+} /* robot_loop */
+
+int main (int argc, char **argv)
+{
     #if SIMULATION
     #else
-    ip = argv[1];
+  ip = argv[1];
     #endif
-    
-    robot_setup();
-    robot_loop();   
+
+  robot_setup();
+  robot_loop();
 }
-
-
-
-
